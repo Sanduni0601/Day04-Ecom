19d5994c7a8d68243d3f4ddd9bcd035105549cf6
Sanduni0601 <sanduni06nisansala@gmail.com>
2025-05-01 11:25:00 +0530

    Initialize project

---
+++ day04/src/test/java/fas/ict/day04/Day04ApplicationTests.java
@@ -0,0 +1,13 @@
+package fas.ict.day04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
